d10cf1e3850a7008c29465ae89f8d72638a9032c
pez <perr@arch.com>
2024-08-13 02:47:48 +0100

    Player 2 added. Player rendering refactored

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tetra::graphics::{self, Color, Texture};
+use tetra::graphics::{self, Color, Texture, DrawParams};
 use tetra::input::{self, Key};
 use tetra::math::Vec2;
 use tetra::{Context, ContextBuilder, State};
@@ -8,21 +8,31 @@ const WINDOW_WIDTH: f32 = 640.0;
 const WINDOW_HEIGHT: f32 = 480.0;
 const PADDLE_SPEED: f32 = 8.0;
 
+struct Player {
+    texture: Texture,
+    params: DrawParams,
+}
+
 struct GameState {
-    paddle_texture: Texture,
-    paddle_position: graphics::DrawParams,
+    player1: Player,
+    player2: Player,
 }
 
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
-        let paddle_texture = Texture::new(ctx, "resources/png/paddleBlu.png")?;
-        let paddle_position = graphics::DrawParams::new()
-            .position(Vec2::new(45.0, WINDOW_HEIGHT / 2.0 - paddle_texture.height() as f32))
+        let player1_texture = Texture::new(ctx, "resources/png/paddleBlu.png")?;
+        let player1_position = DrawParams::new()
+            .position(Vec2::new(45.0, WINDOW_HEIGHT / 2.0 - player1_texture.height() as f32))
+            .rotation(PI /2.0);
+
+        let player2_texture = Texture::new(ctx, "resources/png/paddleRed.png")?;
+        let player2_position = DrawParams::new()
+            .position(Vec2::new(WINDOW_WIDTH - 45.0, WINDOW_HEIGHT / 2.0 - player2_texture.height() as f32))
             .rotation(PI /2.0);
 
         Ok(GameState {
-            paddle_texture,
-            paddle_position,
+            player1: Player::new(player1_texture, player1_position),
+            player2: Player::new(player2_texture, player2_position),
         })
     }
 }
@@ -31,9 +41,35 @@ impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         graphics::clear(ctx, Color::rgb(0.8, 0.4, 0.1));
         
-        self.paddle_texture.draw(ctx, self.paddle_position.clone());
+        self.player1.texture.draw(ctx, self.player1.params.clone());
+        self.player2.texture.draw(ctx, self.player2.params.clone());
 
         Ok(())
+    }
+
+    fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+        if input::is_key_down(ctx, Key::W) {
+            self.player1.params.position.y -= PADDLE_SPEED;
+        }
+
+        if input::is_key_down(ctx, Key::S) {
+            self.player1.params.position.y += PADDLE_SPEED;
+        }
+
+        if input::is_key_down(ctx, Key::Up) {
+            self.player2.params.position.y -= PADDLE_SPEED;
+        }
+
+        if input::is_key_down(ctx, Key::Down) {
+            self.player2.params.position.y += PADDLE_SPEED;
+        }
+        Ok(())
+    }
+}
+
+impl Player {
+    fn new(texture:Texture, params:DrawParams) -> Player {
+        Player{texture, params}
     }
 }
 
