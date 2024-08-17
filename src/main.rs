cf2634f3486fa6d3d67347151644442f58008530
pez <perr@arch.com>
2024-08-17 18:18:34 +0100

    Enough functionality defined for this game to be playable.

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ const WINDOW_WIDTH: f32 = 640.0;
 const WINDOW_HEIGHT: f32 = 480.0;
 const PADDLE_SPEED: f32 = 6.0;
 const BALL_SPEED: f32 = 4.0;
-const PADDLE_SPIN: f32 = 2.0;
 const BALL_ACCELERATION:f32 = 0.05;
+const SPIN_CONSTANT:f32 = 1.5;
 
 struct Player {
     texture: Texture,
@@ -26,7 +26,6 @@ struct GameState {
     player1: Player,
     player2: Player,
     ball: Ball,
-    hits: i32,
 }
 
 // Random player starts. 
@@ -46,6 +45,7 @@ impl GameState {
         let ball_texture = Texture::new(ctx, "resources/png/ballGrey.png")?;
         let ball_position = Vec2::new(
                 WINDOW_WIDTH  / 2.0 - ball_texture.width()  as f32 / 2.0,
+
                 WINDOW_HEIGHT / 2.0 - ball_texture.height() as f32 / 2.0
             );
         let ball_velocity = Vec2::new(BALL_SPEED, 0.0);
@@ -54,7 +54,6 @@ impl GameState {
             player1: Player::new(player1_texture, player1_position),
             player2: Player::new(player2_texture, player2_position),
             ball: Ball::new(ball_texture, ball_position, ball_velocity),
-            hits: 0,
         })
     }
 }
@@ -71,15 +70,6 @@ impl State for GameState {
     }
 
     fn update(&mut self, ctx: &mut Context) -> tetra::Result {
-        if self.ball.position.x <= 0.0 {
-            tetra::window::quit(ctx);
-            println!("Player 2, Wins!");
-        }
-
-        if self.ball.position.x >= WINDOW_WIDTH {
-            tetra::window::quit(ctx);
-            println!("Player1, Wins!");
-        }
 
         self.ball.position += self.ball.velocity;
 
@@ -99,9 +89,9 @@ impl State for GameState {
             self.player2.params.position.y += PADDLE_SPEED;
         }
 
-        let player1_bounds = self.player1.bounds();
-        let player2_bounds = self.player2.bounds();
-        let ball_bounds = self.ball.bounds();
+        let player1_bounds = self.player1.get_bounds();
+        let player2_bounds = self.player2.get_bounds();
+        let ball_bounds = self.ball.get_bounds();
 
         let paddle_hit= if ball_bounds.intersects(&player1_bounds) {
             Some(&self.player1)
@@ -111,27 +101,30 @@ impl State for GameState {
             None
         };
 
-        // Logic for adding spin to ball
-        // Angle of reflection depends on the paddles y velocity 
-        // Spin is commulative (affects angle of reflection from boundaries)
+        // Logic for adding 'spin' to ball
         if let Some(paddle) = paddle_hit {
             self.ball.velocity.x =
                 -(self.ball.velocity.x  + (BALL_ACCELERATION * self.ball.velocity.x.signum()));
 
-            let offset = paddle.centre() - self.ball.centre() / paddle.texture.height() as f32;
+            let offset = (paddle.get_centre() - self.ball.get_centre()) / paddle.texture.height() as f32;
+            self.ball.velocity.y += offset * SPIN_CONSTANT * offset.signum();
 
-            self.ball.velocity.y += PADDLE_SPIN * -offset;
-
-            println!("Offset: {}", offset);
-            //self.hits += 1;
-            println!("Y Speed : {}", self.ball.velocity.y);
         }
+            if self.ball.position.y <= 0.0 || 
+                self.ball.position.y + self.ball.texture.height() as f32 >= WINDOW_HEIGHT {
+                    self.ball.velocity.y = -self.ball.velocity.y;
+            }
 
-        if self.ball.position.y <= 0.0 || 
-            self.ball.position.y + self.ball.texture.height() as f32 >= WINDOW_HEIGHT {
-            self.ball.velocity.y = -self.ball.velocity.y;
-        }
+            if self.ball.position.x <= 0.0 {
+                tetra::window::quit(ctx);
+                println!("Player 2, Wins!");
+            }
 
+            if self.ball.position.x >= WINDOW_WIDTH {
+                tetra::window::quit(ctx);
+                println!("Player1, Wins!");
+            }
+    
         Ok(())
     }
 }
@@ -141,7 +134,7 @@ impl Player {
         Player{texture, params}
     }
 
-    fn bounds(&self) -> Rectangle {
+    fn get_bounds(&self) -> Rectangle {
         Rectangle::new(
             self.params.position.x - self.texture.height() as f32,
             self.params.position.y,
@@ -150,8 +143,8 @@ impl Player {
         )
     }
 
-    fn centre(&self) -> f32 {
-        self.params.position.y + self.texture.height() as f32 / 2.0
+    fn get_centre(&self) -> f32 {
+        (self.texture.height() as f32 / 2.0) + self.params.position.y
     }
 }
 
@@ -160,7 +153,7 @@ impl Ball {
         Ball{texture, position, velocity}
     }
 
-    fn bounds(&self) -> Rectangle {
+    fn get_bounds(&self) -> Rectangle {
         Rectangle::new(
             self.position.x,
             self.position.y,
@@ -169,8 +162,8 @@ impl Ball {
         )
     }
 
-    fn centre(&self) -> f32 {
-        self.position.y + self.texture.height() as f32 / 2.0
+    fn get_centre(&self) -> f32 {
+        (self.texture.height() as f32 / 2.0) + self.position.y
     }
 }
 
